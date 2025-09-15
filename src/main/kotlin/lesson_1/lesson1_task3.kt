e6de99af76bc2be5ae65372e22435544ac5b89f5
askuznetsov <askuznetsov@gmail.com>
2025-09-15 17:14:03 +0300

    KS-1-3 corrected the values of variables

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -3,13 +3,13 @@ package org.example.lesson_1
 fun main() {
 
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
 
     println("Год вылета: $year\nЧас вылета: $hour\nМинута вылета: $minute")
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     println("\nВремя посадки: $hour:$minute")
 
