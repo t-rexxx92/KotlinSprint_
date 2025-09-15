22fda9435eeac580374776d1e493d07c64db62ce
askuznetsov <askuznetsov@gmail.com>
2025-09-15 17:05:59 +0300

    KS-1-3 delete task1 from branch

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -3,7 +3,7 @@ package org.example.lesson_1
 fun main(){
 //    println("Тестовое решение к уроку 1 задаче 1")
 
-    val orderCounter: UInt = 75U
+    val orderCounter: UInt = 75u
     val gratitude: String = "Спасибо за покупку!"
 
     println("Количество заказов: $orderCounter. \nОтветное сообщение на покупку выглядит следующим образом: $gratitude")
@@ -12,6 +12,6 @@ fun main(){
 
 //    println("Количество работников интернет-магазина: $employeeCounter")
 
-    employeeCounter -= 1u
+    employeeCounter--
     println("К сожалению, один работник уволился. Количество работников интернет-магазина теперь: $employeeCounter")
 }
