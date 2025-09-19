067412e309ec1260a3a325860069dbd3380c2811
askuznetsov <askuznetsov@gmail.com>
2025-09-19 17:27:26 +0300

    KS-3-2 strings, Surname changing(alignment)

--- src/main/kotlin/lesson3/task2/Citizen.kt
+++ src/main/kotlin/lesson3/task2/Citizen.kt
@@ -5,30 +5,32 @@ data class AgeSurname(val age: Int, val surname: String)
 class Citizen(
     surname: String,
     val name: String,
-    val patronymic: String,) {
+    val patronymic: String,
+) {
 
-    val zeroAge:AgeSurname  = AgeSurname(0,surname)
+    val zeroAge: AgeSurname = AgeSurname(0, surname)
+
     // пары возрст-фамилия. По умолчанию, с рождения уже есть пара с девечьей фамилией
     val listOfSurnames: MutableList<AgeSurname> = mutableListOf(zeroAge)
 
 
-    fun setNewSurname(age: Int, newSurname: String){
+    fun setNewSurname(age: Int, newSurname: String) {
         val newAgeSurname = AgeSurname(age, newSurname)
         listOfSurnames.addLast(newAgeSurname)
     }
 
 
-    fun getInfo(age: Int){
+    fun getInfo(age: Int) {
         if (listOfSurnames.size > 1) {
             var counter = 1
-            while (listOfSurnames.size != counter){
-                if (listOfSurnames[counter-1].age <= age && listOfSurnames[counter].age > age){
-                    println("ФИО: ${listOfSurnames[counter-1].surname} $name $patronymic\nВозраст: $age")
+            while (listOfSurnames.size != counter) {
+                if (listOfSurnames[counter - 1].age <= age && listOfSurnames[counter].age > age) {
+                    println("ФИО: ${listOfSurnames[counter - 1].surname} $name $patronymic\nВозраст: $age")
                     return
                 }
                 counter++
-                if (counter == listOfSurnames.size){
-                    println("ФИО: ${listOfSurnames[counter-1].surname} $name $patronymic\nВозраст: $age")
+                if (counter == listOfSurnames.size) {
+                    println("ФИО: ${listOfSurnames[counter - 1].surname} $name $patronymic\nВозраст: $age")
                     return
                 }
             }
